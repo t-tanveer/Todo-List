8e22fc2e90962af781e0c864dc470757ef367de7
KingTT <tayyaabtanveer@gmail.com>
2019-11-14 23:30:52 -0500

    First version of app

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,30 @@
-alert("Connected!!");
+$(document).ready(function() {
+
+    //Check Off item on list
+    $("ul").on("click", "li",function(){
+        $(this).toggleClass("checkedOff");
+    });
+
+    //Delete item
+    $("ul").on("click", "span", function(event){
+        $(this).parent().fadeOut(500, function(){
+            $(this).remove();
+        });
+        event.stopPropagation();
+    });
+
+    //Add item with 'enter' keypress
+    $("input[type='text']").keypress(function(event){
+        if(event.which === 13) {
+            var todoText = $(this).val();
+            $(this).val("");
+            $("ul").append("<li> <span> <i class='fa fa-trash'></i> </span>" + todoText + "</li>");            
+        }
+    });
+
+    //Toggle view/edit mode
+    $("#toggle-form").click(function(){
+        $("input[type='text']").fadeToggle();
+    });
+
+});
